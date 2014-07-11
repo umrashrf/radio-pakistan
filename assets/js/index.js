ff59dcecfea833ea5c1e9493d3da4d133aebe9ac
Muhammad Talha Ashraf <talhashraf@gmail.com>
2014-07-11 21:36:21 +0500

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,265 @@
+var radios = [{
+    "stream_url": "http://37.59.196.196:8013/;",
+    "station_name": "FM 108 Pakistan",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fm108pak.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/fm_108_pakistan.png"
+}, {
+    "stream_url": "http://streaming.radionomy.com/Star-FM-Pakistan",
+    "station_name": "Star FM Pakistan",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.star-fm.tk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/star_fm_pakistan.png"
+}, {
+    "stream_url": "http://91.121.242.140:9914/;",
+    "station_name": "Radio Awaz Pakpattan",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radioawaz.com.pk/awaz2012/listen-live/live-107fm-pakpattan/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_awaz_107_fm.png"
+}, {
+    "stream_url": "http://38.96.175.97:5568/;",
+    "station_name": "100 FM Pakistan",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fm100pakistan.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/100_fm_pakistan.png"
+}, {
+    "stream_url": "http://91.121.242.140:9902/;",
+    "station_name": "Radio Awaz FM 105.4",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radioawaz.com.pk/awaz2012/listen-live/105-fm-okara/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_awaz_fm_105_4.png"
+}, {
+    "stream_url": "http://rps.a-xee.com:8000/fm93?type=.flv",
+    "station_name": "Radio Dilber FM 93",
+    "stream_type": "audio/mp4",
+    "station_url": "http://radiodilber.com/radio/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_dilber_fm_.png"
+}, {
+    "stream_url": "http://210.56.24.230:8006/;",
+    "station_name": "FM 101 Hyderabad",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radio.gov.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/fm_101_hyderabad.png"
+}, {
+    "stream_url": "http://198.50.156.215:9310/;",
+    "station_name": "Voice Asia Radio",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://voiceasiaradio.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/voice_asia_radio.png"
+}, {
+    "stream_url": "http://funnypakiradio.mooo.com:8886/;",
+    "station_name": "Funny Paki Radio",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.funnypaki.com",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/funny_paki_radio.png"
+}, {
+    "stream_url": "http://184.154.90.186:8003/live",
+    "station_name": "Riphah FM 102.2",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.riphah.edu.pk",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/riphah_fm_102_2.png"
+}, {
+    "stream_url": "http://38.96.148.106:8096/;",
+    "station_name": "Samaa FM 104.7",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.samaafm.com",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/samaa_fm_104_7.png"
+}, {
+    "stream_url": "http://172.245.24.60:8010/;",
+    "station_name": "Radio Pakmanzil",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://pakmanzil.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_pakmanzil.png"
+}, {
+    "stream_url": "http://217.79.179.162:7600/;",
+    "station_name": "7 Sur Hit FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://7surhitfm.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/7_sur_hit_fm.png"
+}, {
+    "stream_url": "http://204.45.41.148:80/;",
+    "station_name": "Desi Radio",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.desi-radio.com/index.php",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/desi_radio.png"
+}, {
+    "stream_url": "http://217.79.179.162:9980/;",
+    "station_name": "Hot FM 105",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://hotfm.com.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/hot_fm_105_radio.png"
+}, {
+    "stream_url": "http://stream.ewaz.com.pk:7177/;",
+    "station_name": "Hamara FM 98",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.hamarafm.com.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/humara_fm_98.png"
+}, {
+    "stream_url": "http://210.56.24.230:8004/;",
+    "station_name": "Radio 93 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www1.radio.gov.pk/fm93",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_93_fm.png"
+}, {
+    "stream_url": "http://119.152.247.218:8098/fm99",
+    "station_name": "Flare 99 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.flare.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/flare_99_fm.png"
+}, {
+    "stream_url": "http://prclive3.listenon.in:9992/;",
+    "station_name": "FM Karachi Radio",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fmkarachi.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/fm_karachi_radio.png"
+}, {
+    "stream_url": "http://198.50.156.215:9992/;",
+    "station_name": "Sunrise 97 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fmsunrise.biz/#main",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/sunrise_97_fm.png"
+}, {
+    "stream_url": "http://ksa.gulfhost.co:8894/;",
+    "station_name": "Aaj FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.aajfm.com/pakistan-sms.php",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/aaj_fm.png"
+}, {
+    "stream_url": "http://38.96.148.106:8096/;",
+    "station_name": "Samaa FM 107.4",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.samaafm.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/samaa_fm_92_4.png"
+}, {
+    "stream_url": "http://s6.myradiostream.com:12306/;",
+    "station_name": "Spice 107 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.spicefm107.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/spice_107_fm.png"
+}, {
+    "stream_url": "http://91.121.242.140:9918/;",
+    "station_name": "Radio Awaz 104 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radioawaz.com.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_awaz_104_fm.png"
+}, {
+    "stream_url": "http://38.96.148.106:8090/;",
+    "station_name": "Radio Buraq 104 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.buraq.fm/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_buraq_104_fm.png"
+}, {
+    "stream_url": "http://210.56.24.230:8008/;",
+    "station_name": "PBC 101 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www1.radio.gov.pk/fm",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/pbc_585_am.png"
+}, {
+    "stream_url": "http://210.56.24.230:8000/;",
+    "station_name": "NBS Pakistan 639 AM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radio.gov.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/nbs_pakistan_639_am.png"
+}, {
+    "stream_url": "http://210.56.24.230:8030/;",
+    "station_name": "Mirpur 93 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fm93mirpur.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/mirpur_93_fm.png"
+}, {
+    "stream_url": "http://rps.a-xee.com:8000/fsd94?type=.flv",
+    "station_name": "Dhamaal 94 FM",
+    "stream_type": "audio/mp4",
+    "station_url": "http://www.dhamaal94.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/dhammal_94_fm.png"
+}, {
+    "stream_url": "http://91.121.242.140:9920/;",
+    "station_name": "Radio Awaz 106 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radioawaz.com.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_awaz_106_fm.png"
+}, {
+    "stream_url": "http://209.105.250.69:8404/;",
+    "station_name": "Dhak Dhak Radio",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.dhakdhakradio.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/dhak_dhak_radio.png"
+}, {
+    "stream_url": "http://210.56.24.230:8002/;",
+    "station_name": "Planet 94 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://planet94.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/planet_94_fm.png"
+}, {
+    "stream_url": "http://s2.voscast.com:8688/;",
+    "station_name": "Radioactive 96 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radioactive96.fm/ra96/index.html",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radioactive_96_fm.png"
+}, {
+    "stream_url": "http://stream.ewaz.com.pk:7138/live",
+    "station_name": "Apna FM 107",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.apnakarachi107.fm/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/apna_fm_107.png"
+}, {
+    "stream_url": "http://110.34.37.106:8000/live",
+    "station_name": "Zab 106.6 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.zabfm.org/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/zab_106_6_fm.png"
+}, {
+    "stream_url": "http://210.56.24.230:8006/;",
+    "station_name": "Radio FM 101",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.radio.gov.pk",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/radio_fm_101.png"
+}, {
+    "stream_url": "http://151.236.217.111:8000/live",
+    "station_name": "City 89 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://cityfm89.com/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/city_89_fm.png"
+}, {
+    "stream_url": "http://38.96.175.97:5246/;",
+    "station_name": "FM 100",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.fm100pakistan.com",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/fm_100.png"
+}, {
+    "stream_url": "http://3303.live.streamtheworld.com:80/POWER99FM_SC",
+    "station_name": "Power 99 FM",
+    "stream_type": "audio/mpeg",
+    "station_url": "http://www.power99.com.pk/",
+    "station_logo": "http://cdn.radiosfm.org/images/logos/radio_net_pk/power_99_fm.png"
+}];
+
+var index = 0;
+
+var station_name = document.getElementById("rp-station-name");
+station_name.innerHTML = radios[index].station_name;
+
+var stream_url = document.getElementById("rp-stream-url");
+stream_url.src = radios[index].stream_url;
+
+function changeChannel(_index) {
+	if (_index == 0) {
+		if (hasRadio(index - 1)) {
+			index = index - 1;
+		}
+	} else if (_index == 1) {
+		if (hasRadio(index + 1)) {
+			index = index + 1;
+		}
+	}
+
+	station_name.innerHTML = radios[index].station_name;
+	stream_url.src = radios[index].stream_url;
+    stream_url.play();
+}
+
+function hasRadio(_index) {
+	if (!(typeof radios[_index] == 'undefined')) {
+		return true;
+	} return false;
+}
